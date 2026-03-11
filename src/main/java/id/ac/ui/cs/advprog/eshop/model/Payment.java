ddd10f503d5baa450701b697ba06e26d8dc8e27a
mnaufalmuzaki811 <m.naufal.muzaki811@gmail.com>
2026-03-11 09:15:51 +0700

    [GREEN] Implement Payment model validation

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -11,6 +11,45 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
-        // Kerangka kosong
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+
+        if (paymentData == null) {
+            this.status = "REJECTED";
+            return;
+        }
+
+        if ("VOUCHER".equals(method)) {
+            String voucherCode = paymentData.get("voucherCode");
+
+            if (voucherCode != null && voucherCode.length() == 16 && voucherCode.startsWith("ESHOP")) {
+                int numCount = 0;
+                for (char c : voucherCode.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        numCount++;
+                    }
+                }
+                if (numCount == 8) {
+                    this.status = "SUCCESS";
+                } else {
+                    this.status = "REJECTED";
+                }
+            } else {
+                this.status = "REJECTED";
+            }
+        } else if ("BANK_TRANSFER".equals(method)) {
+            String bankName = paymentData.get("bankName");
+            String referenceCode = paymentData.get("referenceCode");
+
+            if (bankName != null && !bankName.trim().isEmpty() &&
+                    referenceCode != null && !referenceCode.trim().isEmpty()) {
+                this.status = "SUCCESS";
+            } else {
+                this.status = "REJECTED";
+            }
+        } else {
+            this.status = "REJECTED";
+        }
     }
 }
